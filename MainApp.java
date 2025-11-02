65fa7908cb363de287d230d70baecad7701395d3
Luis11020504 <luisramos120316@gmail.com>
2025-11-02 16:57:46 -0600

    MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -1,37 +1,23 @@
-package MainApp; // Asumiendo que está en el mismo paquete
+package MainApp;
+
 
 public class MainApp {
-    
-    
-    // El método 'main' es el punto de inicio del programa
     public static void main(String[] args) {
-        
-        // Instanciación de objetos a partir de la clase Producto
-        
-        // 1. Crear el primer objeto (Llama al constructor)
-        Producto laptop = new Producto("Laptop X100", 1200.50, 2);
-        
-        // 2. Crear un segundo objeto, independiente del primero
+
         Producto raton = new Producto("Mouse Inalámbrico", 15.00, 5);
+        raton.mostrarInfo(); // Muestra nombre, precio y cantidad
+        System.out.println("Costo Total de Ratones: $" + raton.calcularTotal());
 
-        // Uso de Métodos: Interactuar con el objeto 'laptop'
-        System.out.println("--- Usando el Objeto Laptop ---");
-        laptop.mostrarInfo();
-        
-        double totalLaptop = laptop.calcularTotal();
-        System.out.println("Costo Total de Laptops: $" + totalLaptop);
+        System.out.println("\n-----------------------------");
 
-        System.out.println("\n--------------------------------");
-        
-        // Uso de Métodos: Interactuar con el objeto 'raton'
-        System.out.println("--- Usando el Objeto Ratón ---");
-        raton.mostrarInfo();
-        
-        double totalRaton = raton.calcularTotal();
-        System.out.println("Costo Total de Ratones: $" + totalRaton);
-        
-        // Total general
-        System.out.println("\n--------------------------------");
-        System.out.println("GRAN TOTAL DE LA ORDEN: $" + (totalLaptop + totalRaton));
+     
+        ProductoElectronico laptop = new ProductoElectronico("Laptop X100", 1200.50, 2, 24);
+        laptop.mostrarInfo(); // Muestra nombre, precio, cantidad y garantía
+        System.out.println("Costo Total de Laptops: $" + laptop.calcularTotal());
+
+        System.out.println("\n-----------------------------");
+
+       
+        System.out.println("GRAN TOTAL DE LA ORDEN: $" + (raton.calcularTotal() + laptop.calcularTotal()));
     }
 }
